6f8c53138225b2a52f3c408bee3031366767a639
Andréa.C <155273473+Andie4@users.noreply.github.com>
2024-05-25 12:59:21 +0200

    Create resaweb.php

---
+++ Resaweb-DeepBlue/resaweb.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Excursions sous marine </title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,551;1,551&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="container espacePageAcceuil">
+    
+            <header>
+                
+                <nav>
+                    <a href="resaweb.php" class="logo">ESM</a>
+                    <ul class="navbar">
+                        <li><a href="notreHistoire.php">Notre histoire</a></li>
+                        <li><a href="excursions.php">Excursions</a></li>
+                        <li><a href="vieMarine.php">Vie marine</a></li>
+                        <li><a href="aPropos.php">A propos</a></li>
+                    </ul>
+                </nav>
+            </header>
+        
+
+
+        <!-- banière  -->
+        <section class="banniere" id="banniere">
+
+            <div class="imgBanniere">
+
+
+                <div class="titreBanniere">
+                    <h1>
+                        <span>EXCURSIONS</span>
+                        <span>SOUS MARINE</span>
+                    </h1>
+
+                </div>
+            </div>
+
+        </section>
+
+
+        <!-- page d'acceuil -->
+        <div class="homePage" id="enSavoirPlus">
+            <img src="https://images.unsplash.com/photo-1602199926649-2e5e447bab97?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="" class="imgPresentation">
+            <div class="partie1">
+                <h2 class="contourBleu sousTitre1"><span>Pourquoi choisir</span><br> <span>DEEP BLUE</span></h2>
+                <h3 class="sousTitre2">Explorez les trésors cachés de l'océan avec <span class="policeBleu">DEEP
+                        BLUE</span></h3>
+                <p class="policeBleu">Là où l'aventure rencontre la beauté, et chaque immersion est une promesse de
+                    découvertes
+                    inoubliables.</p>
+            </div>
+            <!-- <img class="imgPartie1 droite "
+                src="https://media.istockphoto.com/id/181078417/fr/photo/vie-marine-r%C3%A9cif-de-corail.jpg?s=2048x2048&w=is&k=20&c=qKiQLFadkmXwuyt_Nmn3mpgRfTbJ7pP2VcZJwArKl6A="
+                alt="">
+            <img class="imgPartie2 gauche haut"
+                src="https://media.istockphoto.com/id/1481579432/fr/photo/gros-plan-de-deux-plongeurs-heureux-sous-leau-face-%C3%A0-la-cam%C3%A9ra-montrant-le-signe-ok.jpg?s=2048x2048&w=is&k=20&c=so7ggrtpFK23OE3BY3LiSeJfBA117vMmlAY7UvkvQR8="
+                alt=""> -->
+
+        </div>
+        <!-- 
+        <br>
+        <section class="apercu">
+            <div class="onglet">
+                <img src="https://media.istockphoto.com/id/181855014/fr/photo/requin.jpg?s=2048x2048&w=is&k=20&c=ETbuAfy58C6KWNdECmPJna2HtnM7QysEGi4Zl5VF6WU="
+                    alt="Image 1">
+                <p>Description de l'image 1</p>
+            </div>
+            <div class="onglet">
+                <img src="https://media.istockphoto.com/id/482390982/fr/photo/cardinal-poisson-d%C3%A9tail-gros-plan-en-plong%C3%A9e.jpg?s=2048x2048&w=is&k=20&c=oboKSodpOGBH4ntP0GNchlYHnHAXChD_OZUnIQwYyAw="
+                    alt="Image 2">
+                <p>Description de l'image 2</p>
+            </div>
+            <div class="onglet">
+                <img src="https://media.istockphoto.com/id/155372056/fr/photo/subron-6-mouette-tridactyle.jpg?s=2048x2048&w=is&k=20&c=RWXldTtXxtr4t4QrZcit1VlUfyDqkhWw-t5PsNcjHrs="
+                    alt="Image 2">
+                <p>Description de l'image 2</p>
+            </div> -->
+        </section>
+    </div>
+
+    <main class="apercuCatalogue">
+        <section class="apercuArticleCatalogue" >
+            <img src="https://plus.unsplash.com/premium_photo-1682623305315-806515db9c3f?q=80&w=2272&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="" class="imgApercu">
+            <h3 class="sousTitreApercu">1 - Le champs des poissons</h3><br>
+
+        </section>
+        <section class="apercuArticleCatalogue">
+            <img src="https://plus.unsplash.com/premium_photo-1682091874987-687e0c1026b8?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="" class="imgApercu">
+            <h3 class="sousTitreApercu">2 - Les terreurs des mers</h3><br>
+
+        </section>
+        <section class="apercuArticleCatalogue">
+            <img src="https://images.unsplash.com/photo-1598977115413-3b0d09af688c?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="" class="imgApercu">
+            <h3 class="sousTitreApercu">3 - Le monde caché</h3><br>
+        </section>
+    </main>
+    <div class="reserver">
+        <a href="excursions.php " class="lienReserver">Réserver</a>
+    </div>
+
+
+    <footer class="footer">
+        <div class="waves">
+            <div class="wave" id="wave1"></div>
+            <div class="wave" id="wave2"></div>
+            <div class="wave" id="wave3"></div>
+            <div class="wave" id="wave4"></div>
+        </div>
+
+    
+
+        <ul class="menu">
+            <li class="menu__item"><a class="menu__link" href="resaweb.php">Accueil</a></li>
+            <li class="menu__item"><a class="menu__link" href="notreHistoire.php">Notre histoire</a></li>
+            <li class="menu__item"><a class="menu__link" href="excursions.php">Excursions</a></li>
+            <li class="menu__item"><a class="menu__link" href="vieMarine.php">Vie marine</a></li>
+            <li class="menu__item"><a class="menu__link" href="aPropos.php">A propos</a></li>
+            <li class="menu__item"><a class="menu__link" href="#">Recherche</a></li>
+        </ul>
+
+        <p>&copy;2024 Caneval Andréa | Tous droits réservés</p>
+    </footer>
+
+
+
+
+    <script src="resaweb.js"></script>
+</body>
+
+</html>
