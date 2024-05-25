66eda20743889c9499b4af32b5219cb593afa5c8
Andréa.C <155273473+Andie4@users.noreply.github.com>
2024-05-26 00:45:32 +0200

    Update resaweb.php

--- Resaweb-DeepBlue/resaweb.php
+++ Resaweb-DeepBlue/resaweb.php
@@ -32,21 +32,15 @@
 
 
         <!-- banière  -->
-        <section class="banniere" id="banniere">
-
-            <div class="imgBanniere">
-
-
-                <div class="titreBanniere">
-                    <h1>
+        
+<div class="imgBanniere" >
+                    <h1 class="titreBanniere">
                         <span>EXCURSIONS</span>
                         <span>SOUS MARINE</span>
                     </h1>
+</div>
 
-                </div>
-            </div>
-
-        </section>
+</section>
 
 
         <!-- page d'acceuil -->
@@ -61,52 +55,29 @@
                     découvertes
                     inoubliables.</p>
             </div>
-            <!-- <img class="imgPartie1 droite "
-                src="https://media.istockphoto.com/id/181078417/fr/photo/vie-marine-r%C3%A9cif-de-corail.jpg?s=2048x2048&w=is&k=20&c=qKiQLFadkmXwuyt_Nmn3mpgRfTbJ7pP2VcZJwArKl6A="
-                alt="">
-            <img class="imgPartie2 gauche haut"
-                src="https://media.istockphoto.com/id/1481579432/fr/photo/gros-plan-de-deux-plongeurs-heureux-sous-leau-face-%C3%A0-la-cam%C3%A9ra-montrant-le-signe-ok.jpg?s=2048x2048&w=is&k=20&c=so7ggrtpFK23OE3BY3LiSeJfBA117vMmlAY7UvkvQR8="
-                alt=""> -->
+
 
         </div>
-        <!-- 
-        <br>
-        <section class="apercu">
-            <div class="onglet">
-                <img src="https://media.istockphoto.com/id/181855014/fr/photo/requin.jpg?s=2048x2048&w=is&k=20&c=ETbuAfy58C6KWNdECmPJna2HtnM7QysEGi4Zl5VF6WU="
-                    alt="Image 1">
-                <p>Description de l'image 1</p>
-            </div>
-            <div class="onglet">
-                <img src="https://media.istockphoto.com/id/482390982/fr/photo/cardinal-poisson-d%C3%A9tail-gros-plan-en-plong%C3%A9e.jpg?s=2048x2048&w=is&k=20&c=oboKSodpOGBH4ntP0GNchlYHnHAXChD_OZUnIQwYyAw="
-                    alt="Image 2">
-                <p>Description de l'image 2</p>
-            </div>
-            <div class="onglet">
-                <img src="https://media.istockphoto.com/id/155372056/fr/photo/subron-6-mouette-tridactyle.jpg?s=2048x2048&w=is&k=20&c=RWXldTtXxtr4t4QrZcit1VlUfyDqkhWw-t5PsNcjHrs="
-                    alt="Image 2">
-                <p>Description de l'image 2</p>
-            </div> -->
         </section>
     </div>
 
     <main class="apercuCatalogue">
-        <section class="apercuArticleCatalogue" >
+        <section class="apercuArticleCatalogue un" >
             <img src="https://plus.unsplash.com/premium_photo-1682623305315-806515db9c3f?q=80&w=2272&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="" class="imgApercu">
-            <h3 class="sousTitreApercu">1 - Le champs des poissons</h3><br>
+            <h3 class="sousTitreApercu">Le champs des poissons</h3><br>
 
         </section>
-        <section class="apercuArticleCatalogue">
+        <section class="apercuArticleCatalogue deux">
             <img src="https://plus.unsplash.com/premium_photo-1682091874987-687e0c1026b8?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="" class="imgApercu">
-            <h3 class="sousTitreApercu">2 - Les terreurs des mers</h3><br>
+            <h3 class="sousTitreApercu">Les terreurs des mers</h3><br>
 
         </section>
-        <section class="apercuArticleCatalogue">
+        <section class="apercuArticleCatalogue trois">
             <img src="https://images.unsplash.com/photo-1598977115413-3b0d09af688c?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="" class="imgApercu">
-            <h3 class="sousTitreApercu">3 - Le monde caché</h3><br>
+            <h3 class="sousTitreApercu">Le monde caché</h3><br>
         </section>
     </main>
     <div class="reserver">
