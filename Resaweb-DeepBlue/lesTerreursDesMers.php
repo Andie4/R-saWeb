5e6a77ca699c794923dfbed5d02652ab1f7dbbd4
Andréa.C <155273473+Andie4@users.noreply.github.com>
2024-06-07 23:41:51 +0200

    Update lesTerreursDesMers.php

--- Resaweb-DeepBlue/lesTerreursDesMers.php
+++ Resaweb-DeepBlue/lesTerreursDesMers.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Deep Blue</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="images/meduse.png">
 </head>
 <body>
     <div class="container espacePageAcceuil"></div>
