d045cdaf97744ef15f3132740c5848779f6d079e
Andréa.C <155273473+Andie4@users.noreply.github.com>
2024-06-07 23:42:08 +0200

    Update laDanseDesMeduses.php

--- Resaweb-DeepBlue/laDanseDesMeduses.php
+++ Resaweb-DeepBlue/laDanseDesMeduses.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">    
+    <title>Deep Blue</title>
+    <link rel="stylesheet" href="style.css">  
+    <link rel="icon" href="images/meduse.png">  
 </head>
 <body>
     <div class="container espacePageAcceuil"></div>
