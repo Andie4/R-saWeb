55014d92dc064a9e8574e273a6649b7d549350f1
Andréa.C <155273473+Andie4@users.noreply.github.com>
2024-06-07 23:41:28 +0200

    Update vieMarine.php

--- Resaweb-DeepBlue/vieMarine.php
+++ Resaweb-DeepBlue/vieMarine.php
@@ -10,6 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,551;1,551&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="images/meduse.png">
 </head>
 
 <body>
@@ -32,26 +33,25 @@
 
     <section class="vieMarine">
       <h1 class="titrePages">Vie marine</h1>
-          <div class="containeurBandeau"><img src="https://images.unsplash.com/photo-1516979371633-d578f00fcd6e?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="bandeau"><img src="https://plus.unsplash.com/premium_photo-1685036741861-bb3a2ebd4988?q=80&w=3028&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="bandeau"><img src="https://plus.unsplash.com/premium_photo-1661963118875-6d1363a48560?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="bandeau"><img src="https://images.unsplash.com/photo-1692285736030-03fb3ba9cd18?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="bandeau"></div>
+          
         <h2 class="couleurSousTitreVieMarine">Préserver la Vie Marine : Nos Engagements et Vos Responsabilités</h2><br>
         <p class="paragrapheCentrer">Chez DeepBlue, nous croyons en la nécessité de protéger et de préserver la vie
             marine lors de nos excursions sous-marines. En tant qu'amoureux des océans, il est de notre devoir de
             veiller à ce que nos actions aient le moins d'impact possible sur les écosystèmes marins fragiles que nous
-            avons la chance de visiter. Voici quelques précautions et règles importantes à suivre pour garantir une
-            expérience sous-marine respectueuse et durable.</p>
+            avons la chance de visiter. Voici quelques précautions et règles importantes à suivre pour garantir <span class="surlignement">une expérience sous-marine respectueuse et durable.</span></p>
 
         <br><br>
 
         <h3 class="titresParagraphes couleurSousTitreVieMarine">Respectez la Vie Marine :</h3><br>
         <p class="paragraphe">Lors de vos plongées, veillez à ne pas perturber ou déranger la faune marine. Gardez une
-            distance sécuritaire avec les animaux et évitez de les toucher ou de les chasser. Observer les créatures
+            distance sécuritaire avec les animaux et <span class="surlignement">évitez de les toucher ou de les chasser.</span> Observer les créatures
             marines dans leur habitat naturel est déjà une expérience extraordinaire en soi, pas besoin d'interférer
             avec elles.</p>
 
         <br><br>
 
         <h3 class="titresParagraphes couleurSousTitreVieMarine">Ne Laissez Aucune Trace :</h3><br>
-        <p class="paragraphe">Lorsque vous plongez, assurez-vous de ne laisser aucun déchet derrière vous. Ramenez
+        <p class="paragraphe">Lorsque vous plongez, assurez-vous de<span class="surlignement"> ne laisser aucun déchet derrière vous.</span>  Ramenez
             toujours vos déchets à la surface et disposez-les de manière responsable. Même les déchets biologiques,
             comme les pelures de fruits, ne devraient pas être jetés dans l'océan, car ils peuvent perturber l'équilibre
             écologique.</p>
@@ -61,30 +61,28 @@
         <h3 class="titresParagraphes couleurSousTitreVieMarine">Évitez les Produits Chimiques Nocifs :</h3><br>
         <p class="paragraphe">Lorsque vous appliquez des produits comme la crème solaire ou les lotions anti-moustiques
             avant votre plongée, choisissez des produits respectueux de l'environnement. Certains produits chimiques
-            contenus dans les produits de soins personnels peuvent être nocifs pour les récifs coralliens et les
-            organismes marins. Optez pour des options sans danger pour la vie marine.</p>
+            contenus dans <span class="surlignement">les produits de soins personnels peuvent être nocifs pour les récifs coralliens et les
+            organismes marins.</span> Optez pour des options sans danger pour la vie marine.</p>
 
         <br><br>
 
         <h3 class="titresParagraphes couleurSousTitreVieMarine">Respectez les Règles Locales :</h3><br>
         <p class="paragraphe">Chaque destination de plongée peut avoir ses propres règles et réglementations pour
             protéger la vie marine. Assurez-vous de vous renseigner sur les règles locales avant votre plongée et
-            suivez-les scrupuleusement. Ces règles sont mises en place pour préserver les écosystèmes marins et garantir
-            la sécurité de tous les plongeurs.</p>
+            suivez-les scrupuleusement. Ces règles sont mises en place pour préserver les écosystèmes marins et garantir <span class="surlignement">la sécurité de tous les plongeurs.</span>
+            </p>
 
         <br><br>
 
         <h3 class="titresParagraphes couleurSousTitreVieMarine">Sensibilisation et Éducation :</h3><br>
         <p class="paragraphe">Partagez vos connaissances sur la préservation de la vie marine avec vos compagnons de
-            plongée et les personnes que vous rencontrez. Plus nous sommes nombreux à prendre conscience de l'importance
-            de préserver les océans, plus nous pourrons agir efficacement pour leur protection.</p>
+            plongée et les personnes que vous rencontrez. Plus nous sommes nombreux à prendre conscience de l'importance de <span class="surlignement"> préserver les océans</span>
+           , plus nous pourrons agir efficacement pour leur protection.</p>
 
         <br><br>
 
-        <p class="conclusionVieMarine">Ensemble, en respectant ces précautions et en suivant ces règles, nous pouvons
-            contribuer à préserver la vie marine et à garantir que les générations futures puissent également profiter
-            de la beauté des océans. Merci de faire partie de cet engagement en faveur de la conservation marine.</p>
-            <div class="containeurBandeau"><img src="https://images.unsplash.com/photo-1516979371633-d578f00fcd6e?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="bandeau"><img src="https://plus.unsplash.com/premium_photo-1685036741861-bb3a2ebd4988?q=80&w=3028&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="bandeau"><img src="https://plus.unsplash.com/premium_photo-1661963118875-6d1363a48560?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="bandeau"><img src="https://images.unsplash.com/photo-1692285736030-03fb3ba9cd18?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="bandeau"></div>
+        <p class="conclusionVieMarine">Ensemble, en respectant ces précautions et en suivant ces règles, nous pouvons contribuer à préserver la vie marine et à garantir que les générations futures puissent également profiter de la beauté des océans. Merci de faire partie de cet engagement en faveur de la conservation marine.</p>
+            
     </section>
 
     <footer class="footer">
