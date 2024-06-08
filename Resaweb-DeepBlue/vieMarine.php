44ff806010539e5c0a7cbf531472d0e75e391df0
Andréa.C <155273473+Andie4@users.noreply.github.com>
2024-06-09 01:23:13 +0200

    Update vieMarine.php

--- Resaweb-DeepBlue/vieMarine.php
+++ Resaweb-DeepBlue/vieMarine.php
@@ -11,6 +11,7 @@
         rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <link rel="icon" href="images/meduse.png">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 </head>
 
 <body>
@@ -33,55 +34,70 @@
 
     <section class="vieMarine">
       <h1 class="titrePages">Vie marine</h1>
-          
-        <h2 class="couleurSousTitreVieMarine">Préserver la Vie Marine : Nos Engagements et Vos Responsabilités</h2><br>
+      <div data-aos="fade-up">
+         <h2 class="couleurSousTitreVieMarine">Préserver la Vie Marine : Nos Engagements et Vos Responsabilités</h2><br>
         <p class="paragrapheCentrer">Chez DeepBlue, nous croyons en la nécessité de protéger et de préserver la vie
             marine lors de nos excursions sous-marines. En tant qu'amoureux des océans, il est de notre devoir de
             veiller à ce que nos actions aient le moins d'impact possible sur les écosystèmes marins fragiles que nous
             avons la chance de visiter. Voici quelques précautions et règles importantes à suivre pour garantir <span class="surlignement">une expérience sous-marine respectueuse et durable.</span></p>
 
+      </div>
+       
         <br><br>
-
-        <h3 class="titresParagraphes couleurSousTitreVieMarine">Respectez la Vie Marine :</h3><br>
+        <div data-aos="fade-up">
+            <h3 class="titresParagraphes couleurSousTitreVieMarine">Respectez la Vie Marine :</h3><br>
         <p class="paragraphe">Lors de vos plongées, veillez à ne pas perturber ou déranger la faune marine. Gardez une
             distance sécuritaire avec les animaux et <span class="surlignement">évitez de les toucher ou de les chasser.</span> Observer les créatures
             marines dans leur habitat naturel est déjà une expérience extraordinaire en soi, pas besoin d'interférer
             avec elles.</p>
+        </div>
+        
 
         <br><br>
-
-        <h3 class="titresParagraphes couleurSousTitreVieMarine">Ne Laissez Aucune Trace :</h3><br>
+        <div data-aos="fade-up">
+            <h3 class="titresParagraphes couleurSousTitreVieMarine">Ne Laissez Aucune Trace :</h3><br>
         <p class="paragraphe">Lorsque vous plongez, assurez-vous de<span class="surlignement"> ne laisser aucun déchet derrière vous.</span>  Ramenez
             toujours vos déchets à la surface et disposez-les de manière responsable. Même les déchets biologiques,
             comme les pelures de fruits, ne devraient pas être jetés dans l'océan, car ils peuvent perturber l'équilibre
             écologique.</p>
+        </div>
+        
 
         <br><br>
-
-        <h3 class="titresParagraphes couleurSousTitreVieMarine">Évitez les Produits Chimiques Nocifs :</h3><br>
+        <div data-aos="fade-up">
+            <h3 class="titresParagraphes couleurSousTitreVieMarine">Évitez les Produits Chimiques Nocifs :</h3><br>
         <p class="paragraphe">Lorsque vous appliquez des produits comme la crème solaire ou les lotions anti-moustiques
             avant votre plongée, choisissez des produits respectueux de l'environnement. Certains produits chimiques
             contenus dans <span class="surlignement">les produits de soins personnels peuvent être nocifs pour les récifs coralliens et les
             organismes marins.</span> Optez pour des options sans danger pour la vie marine.</p>
+        </div>
+        
 
         <br><br>
-
-        <h3 class="titresParagraphes couleurSousTitreVieMarine">Respectez les Règles Locales :</h3><br>
+        <div data-aos="fade-up">
+            <h3 class="titresParagraphes couleurSousTitreVieMarine">Respectez les Règles Locales :</h3><br>
         <p class="paragraphe">Chaque destination de plongée peut avoir ses propres règles et réglementations pour
             protéger la vie marine. Assurez-vous de vous renseigner sur les règles locales avant votre plongée et
             suivez-les scrupuleusement. Ces règles sont mises en place pour préserver les écosystèmes marins et garantir <span class="surlignement">la sécurité de tous les plongeurs.</span>
             </p>
+        </div>
+        
 
         <br><br>
-
-        <h3 class="titresParagraphes couleurSousTitreVieMarine">Sensibilisation et Éducation :</h3><br>
+        <div data-aos="fade-up">
+            <h3 class="titresParagraphes couleurSousTitreVieMarine">Sensibilisation et Éducation :</h3><br>
         <p class="paragraphe">Partagez vos connaissances sur la préservation de la vie marine avec vos compagnons de
             plongée et les personnes que vous rencontrez. Plus nous sommes nombreux à prendre conscience de l'importance de <span class="surlignement"> préserver les océans</span>
            , plus nous pourrons agir efficacement pour leur protection.</p>
+        </div>
+        
 
         <br><br>
 
-        <p class="conclusionVieMarine">Ensemble, en respectant ces précautions et en suivant ces règles, nous pouvons contribuer à préserver la vie marine et à garantir que les générations futures puissent également profiter de la beauté des océans. Merci de faire partie de cet engagement en faveur de la conservation marine.</p>
+        <div data-aos="zoom-in">
+            <p class="conclusionVieMarine">Ensemble, en respectant ces précautions et en suivant ces règles, nous pouvons contribuer à préserver la vie marine et à garantir que les générations futures puissent également profiter de la beauté des océans. Merci de faire partie de cet engagement en faveur de la conservation marine.</p>
+        </div>
+            
             
     </section>
 
@@ -108,7 +124,11 @@
     <script src="resaweb.js"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <!--Pour initialiser le plugin AOS -->
+    <script>
+        AOS.init();
+    </script>   
 </body>
 
 </html>
